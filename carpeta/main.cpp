a91b21c82f5b8086de8d7e78a46ed9b2d1b5eef2
kato <yhlx05@gmail.com>
2025-04-07 15:07:38 -0500

    Primero para EDO

---
+++ carpeta/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+  int valor;
+  cin >> valor;
+  for (int i = 0; i < valor; i++) {
+    cout << i << endl;
+  }
+  cout << "Hello";
+  cout << endl;
+  return 0;
+}
